1f89db68c04fc0cfa13d0598f6342bb22dbf81d0
Martisum <2257546277@qq.com>
2020-05-02 11:56:52 +0800

    '20200502'

--- Update.bat
+++ Update.bat
@@ -1,5 +1,5 @@
 git add .
-git commit -m '测试能否快速上传'
+git commit -m '20200502'
 git remote add origin https://github.com/Martisum/Documents.git
 git pull --rebase origin master
 git push -u origin master
